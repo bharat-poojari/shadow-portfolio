a6e9037acc2c7704e6eb15efc4c539c22b72193c
bharat-poojari <bharatp0316@gmail.com>
2026-09-09 10:36:00 +0530

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta
+          name="google-site-verification"
+          content="8IsUEadK0DA_EARkB89dOJkUj0MMbVKmujCqq_FVUeE"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
